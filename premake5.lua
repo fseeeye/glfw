bb832d3dbab30e0f8e3e4276926c08a38ad1f27b
fseeeye <fseeeye@gmail.com>
2021-12-29 15:37:03 +0800

    fix: link error caused by missing files in premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,5 @@
+-- ref: https://github.com/TheCherno/glfw
+-- ref: https://stackoverflow.com/questions/70199224/trouble-linking-with-glfw-using-premake-and-vs2019
 project "GLFW"
 	kind "StaticLib"
 	language "C"
@@ -9,14 +11,27 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
 	}
+
 	filter "system:linux"
 		pic "On"
 
@@ -46,12 +61,19 @@ project "GLFW"
 		systemversion "latest"
 		staticruntime "On"
 
+		-- buildoptions{
+		-- 	"/MT"
+		-- }
+
 		files
 		{
 			"src/win32_init.c",
+			"src/win32_module.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
+			"src/win32_time.h",
 			"src/win32_time.c",
+			"src/win32_thread.h",
 			"src/win32_thread.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
